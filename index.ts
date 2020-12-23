bd4cfe5e70bc523ca3e958d6b022134d88070afa
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-12-23 14:24:31 +0000

    1.修改

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer"
 import Tesseract from "tesseract.js"
+import fs from "fs"
 
 const worker = Tesseract.createWorker();
 
@@ -17,23 +18,45 @@ const worker = Tesseract.createWorker();
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4356.0 Safari/537.36")
     await page.goto('https://cd.zu.anjuke.com/fangyuan/x1-p2/', { waitUntil: 'domcontentloaded' });
-    
-    let elements = await page.$$(".zu-itemmod")
-    let ua = await x.userAgent()
-    let i = 1
-    for(let element of elements){
-        let address = await element.$eval("address", node => node.textContent)
-        if(address == null) continue
-        address = address.replace(/\s+/g, " ").trim()
-        console.log(address, i++)
+    let data= await page.evaluate(function(){
+        let elements = document.querySelectorAll(".zu-itemmod")
+        let res = []
+        for(let element of elements){
+            let str = element.querySelector("address.details-item")?.childNodes[2].nodeValue?.trim() || ""
+            let [pre, local] = str.split(" ")
+            let [qu, range] = pre.split("-")
+            str = qu.replace("区", "") + "区" + local
+            res.push(str)
+        }
+        return res.join("\n")
+    })
+    fs.writeFileSync("./location.txt", data, "utf-8")
 
-        let priceE = await element.$(".zu-side strong")
-        if(priceE == null) continue
-        console.log(await getElementNumber(priceE))
-        let sizeE = await element.$(".details-item.tag .strongbox:nth-of-type(3)")
-        if(sizeE == null) continue
-        console.log(await getElementNumber(sizeE))
-    }
+    
+    // let elements = await page.$$(".zu-itemmod")
+    // let ua = await x.userAgent()
+    // let i = 1
+    // for(let element of elements){
+    //     // let address = await element.$eval("address", node => node.textContent)
+    //     // if(address == null) continue
+    //     // address = address.replace(/\s+/g, " ").trim()
+    //     // console.log(address, i++)
+    //     let data= await element.evaluate(function(){
+    //         let x = document.querySelectorAll(".zu-itemmod")
+    //         console.log(x)
+    //         return x.length
+    //     })
+    //     console.log(data)
+        
+
+
+    //     // let priceE = await element.$(".zu-side strong")
+    //     // if(priceE == null) continue
+    //     // console.log(await getElementNumber(priceE))
+    //     // let sizeE = await element.$(".details-item.tag .strongbox:nth-of-type(3)")
+    //     // if(sizeE == null) continue
+    //     // console.log(await getElementNumber(sizeE))
+    // }
 
     await browser.close();
     await worker.terminate();
